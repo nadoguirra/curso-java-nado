89a5901bed8342310a06dcdec3ba63e56b07c455
nadoguirra <edvaldo@saol.com.br>
2016-05-07 18:57:47 -0300

    Testando o Git-cola RicardoRX
    Testando o Git-cola na casa de RicardoRX. O Puxar funcionou. Agora
    estamos comitando e reenviando os arquivos alterados e adicionados.

---
+++ aula0006_RicardoRX.java
@@ -0,0 +1,15 @@
+package aula0006;
+
+public class aula0006 {
+	
+	public static void main(String[] args){
+		
+		int num1, num2;
+		
+		num1 = 100;
+		num2 = num1 + 100;
+		
+		System.out.println( "O resusltado é: "+num2 );
+		System.out.println( "Num1 é igual: "+num1 );		
+	}
+}
--- readme.md
+++ readme.md
@@ -4,4 +4,10 @@ Vídeos aulas são chatas!!!
 
 Vamos ver se o Git-cola torna isso agradável.
 
+Na casa de RicardoRX o Git-cola fez o puxar.
+
+Vou agora fazer o comit apenas do update sem novos arquivos.
+
+
+
 
